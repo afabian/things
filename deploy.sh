c09249366def6792d4818f9a5a32a408421ca2b5
Andy Fabian <andrew.fabian@gmail.com>
2026-05-10 21:22:45 -0700

    Switch deployment to git pull; wsserver runs from git clone

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 set -e
 SERVER=10.0.0.10
-DEST=/var/www/html/things
 
-echo "Deploying to $SERVER:$DEST ..."
-rsync -a --delete \
-    --exclude='parsed/' \
-    --exclude='uploads/' \
-    --exclude='*.md' \
-    /home/afabian/things/www/ \
-    $SERVER:$DEST/
+echo "Pulling latest on server ..."
+ssh $SERVER "cd ~/things && git pull"
 
-echo "Setting up server directories ..."
-ssh $SERVER "
-    mkdir -p $DEST/parsed/dev $DEST/parsed/prod $DEST/uploads
-    chmod 777 $DEST/parsed $DEST/parsed/dev $DEST/parsed/prod $DEST/uploads
-"
+echo "Rebuilding WebSocket server ..."
+ssh $SERVER "cd ~/things/wsserver && cmake --build build"
+
+echo "Restarting WebSocket server ..."
+ssh $SERVER "sudo systemctl restart things-ws"
 
 echo "Done. http://$SERVER/things/"
